3343c69e6ba1f90244ae7bf63164664060686626
nahitferhat <ferhat.ektas@marsathletic.com>
2024-08-20 22:44:38 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
-import { BehaviorSubject, combineLatest, map, tap } from 'rxjs';
+import {
+  BehaviorSubject,
+  combineLatest,
+  debounceTime,
+  distinctUntilChanged,
+  map,
+  shareReplay,
+  tap,
+} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -42,6 +50,22 @@ import { BehaviorSubject, combineLatest, map, tap } from 'rxjs';
             </p>
           </div>
         </div>
+        <div class="py-1 px-5">
+          <div class="flex items-center justify-between">
+            <h5 class="font-thin">Total Price:</h5>
+            <p class="text-orange-400 font-semibold">
+              {{ totalPrice$ | async }} $
+            </p>
+          </div>
+        </div>
+        <div class="py-1 px-5">
+          <div class="flex items-center justify-between">
+            <h5 class="font-thin">Total Price:</h5>
+            <p class="text-orange-400 font-semibold">
+              {{ totalPrice$ | async }} $
+            </p>
+          </div>
+        </div>
         <button
           (click)="calculate()"
           class="inline-flex mt-5 px-5 w-full py-2 text-sm font-medium justify-center text-white bg-orange-400 rounded-lg hover:bg-orange-500 focus:ring-4 focus:outline-none"
@@ -60,15 +84,17 @@ export class AppComponent implements OnInit {
   quantity$ = new BehaviorSubject(4);
   discount$ = new BehaviorSubject(50);
   totalPrice$ = combineLatest([
-    this.price$,
-    this.quantity$,
-    this.discount$,
+    this.price$.pipe(distinctUntilChanged()),
+    this.quantity$.pipe(distinctUntilChanged()),
+    this.discount$.pipe(distinctUntilChanged()),
   ]).pipe(
+    debounceTime(0),
     tap(() => this.counter++),
     map(([price, quantity, discount]) => {
       const total = price * quantity;
       return total - (total * discount) / 100;
-    })
+    }),
+    shareReplay()
   );
 
   ngOnInit(): void {
